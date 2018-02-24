cb815a8b244741a556f6c3e54431dfd85ba89f5e
David Honisch <david@honisch.org>
2018-02-24 22:57:57 +0100

    LC2FFMPEG.Convert - FFMPEG Open Source Code - Build in Visual Basic .NET

---
+++ LC2FFMPEG.Convert/EncoderSample/EncoderSample/Form1.Designer.vb
@@ -0,0 +1,179 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ProgressBar = New System.Windows.Forms.ProgressBar()
+        Me.StatusLbl = New System.Windows.Forms.Label()
+        Me.ListBox1 = New System.Windows.Forms.CheckedListBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ProgressBar
+        '
+        Me.ProgressBar.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ProgressBar.Location = New System.Drawing.Point(2, 493)
+        Me.ProgressBar.Name = "ProgressBar"
+        Me.ProgressBar.Size = New System.Drawing.Size(799, 23)
+        Me.ProgressBar.TabIndex = 0
+        '
+        'StatusLbl
+        '
+        Me.StatusLbl.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.StatusLbl.AutoSize = True
+        Me.StatusLbl.Location = New System.Drawing.Point(12, 477)
+        Me.StatusLbl.Name = "StatusLbl"
+        Me.StatusLbl.Size = New System.Drawing.Size(40, 13)
+        Me.StatusLbl.TabIndex = 1
+        Me.StatusLbl.Text = "Status:"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.AllowDrop = True
+        Me.ListBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(183, 27)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(618, 469)
+        Me.ListBox1.TabIndex = 11
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button3)
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Location = New System.Drawing.Point(2, 27)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(175, 70)
+        Me.GroupBox1.TabIndex = 14
+        Me.GroupBox1.TabStop = False
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(5, 2)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(164, 28)
+        Me.Button3.TabIndex = 24
+        Me.Button3.Text = "Convert to audio file"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button2.Location = New System.Drawing.Point(6, 36)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(164, 28)
+        Me.Button2.TabIndex = 23
+        Me.Button2.Text = "Convert to MKV"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(2, 3)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(27, 20)
+        Me.Button1.TabIndex = 21
+        Me.Button1.Text = "..."
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(30, 3)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(771, 20)
+        Me.TextBox1.TabIndex = 15
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Button4)
+        Me.GroupBox2.Controls.Add(Me.Button5)
+        Me.GroupBox2.Location = New System.Drawing.Point(2, 404)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(175, 70)
+        Me.GroupBox2.TabIndex = 22
+        Me.GroupBox2.TabStop = False
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(5, 2)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(164, 28)
+        Me.Button4.TabIndex = 24
+        Me.Button4.Text = "Update"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button5.Location = New System.Drawing.Point(6, 36)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(164, 28)
+        Me.Button5.TabIndex = 23
+        Me.Button5.Text = "Close"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(802, 520)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.StatusLbl)
+        Me.Controls.Add(Me.ProgressBar)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "LC2FFMPEG Encoder"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ProgressBar As System.Windows.Forms.ProgressBar
+    Friend WithEvents StatusLbl As System.Windows.Forms.Label
+    Friend WithEvents ListBox1 As System.Windows.Forms.CheckedListBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+
+End Class
---
+++ LC2FFMPEG.Convert/EncoderSample/EncoderSample/Form1.vb
@@ -0,0 +1,141 @@
+﻿Public Class Form1
+    Public WithEvents newcoder As New FFLib.Encoder
+    Private Sub ConOut(ByVal prog As String, ByVal tl As String) Handles newcoder.Progress
+        ProgressBar.Value = prog
+        Application.DoEvents()
+    End Sub
+    Private Sub stat(ByVal status) Handles newcoder.Status
+        StatusLbl.Text = status
+        Application.DoEvents()
+    End Sub
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        Dim outpath As New FolderBrowserDialog
+        If outpath.ShowDialog = Windows.Forms.DialogResult.OK Then
+            outfolder = outpath.SelectedPath
+            TextBox1.Text = outpath.SelectedPath
+        End If
+    End Sub
+    Dim outfolder
+    Private Sub ListBox1_DragEnter(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DragEventArgs) Handles ListBox1.DragEnter
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then 'makes sure its a file or folder
+            e.Effect = DragDropEffects.Copy 'copys the item to the "e" handler for later use 
+            'in the drag drop event
+        Else
+            e.Effect = DragDropEffects.None 'if its not a file or folder, do nothing
+            'actually it will display a circle with line "not valid"
+        End If
+    End Sub
+    Private Sub ListBox1_DragDrop(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DragEventArgs) Handles ListBox1.DragDrop
+        Dim file_names As String() = DirectCast(e.Data.GetData(DataFormats.FileDrop), String())
+        'declares list of strings called "file_names". contains all the items dropped into list box       
+        Dim listofiles As New ArrayList
+        For Each file_name As String In file_names
+            If My.Computer.FileSystem.FileExists(file_name) = True Then
+                'a file was dragged
+                listofiles.Add(file_name)
+            ElseIf My.Computer.FileSystem.DirectoryExists(file_name) = True Then
+                'a folder was added..
+                Try
+                    listofiles.AddRange(My.Computer.FileSystem.GetFiles(file_name, FileIO.SearchOption.SearchAllSubDirectories))
+                Catch ex As Exception
+                End Try
+            End If
+        Next file_name
+        For Each file In listofiles
+            'Create a file info object
+            Dim FileInfo As New System.IO.FileInfo(file)
+            If My.Settings.KnownFiles.Contains(FileInfo.Extension) = True Then
+                'trim extension off of filename.
+                ListBox1.Items.Add(file)
+            End If
+        Next
+    End Sub
+
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim Newarray As New ArrayList
+        My.Settings.KnownFiles = Newarray
+        My.Settings.KnownFiles.Add(".VOB")
+        My.Settings.KnownFiles.Add(".wmv")
+        My.Settings.KnownFiles.Add(".avi")
+        My.Settings.KnownFiles.Add(".m2ts")
+        My.Settings.KnownFiles.Add(".MP4")
+        My.Settings.KnownFiles.Add(".ASF")
+        My.Settings.KnownFiles.Add(".flac")
+        My.Settings.KnownFiles.Add(".vob")
+        My.Settings.KnownFiles.Add(".flv")
+        My.Settings.KnownFiles.Add(".mp3")
+    End Sub
+
+    Private Sub Button2_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        For index = 0 To ListBox1.Items.Count - 1
+            newcoder.OverWrite = False
+            newcoder.SourceFile = ListBox1.Items.Item(index)
+            newcoder.Format = newcoder.Format_MKV
+            newcoder.AudioCodec = newcoder.AudioCodec_mp3
+            newcoder.AudioBitrate = "128k"
+            newcoder.Video_Codec = newcoder.Vcodec_h264
+            newcoder.Threads = 0
+            newcoder.OverWrite = True
+            '  newcoder.ConstantRateFactor = 22
+            newcoder.RateControl = newcoder.RateControl_ABR
+            newcoder.VideoBitrate = "1000k"
+
+            newcoder.Libx264_Preset_pass1 = newcoder.libx264_fast
+
+            newcoder.OutputPath = outfolder
+            newcoder.AnalyzeFile()
+
+            newcoder.Encode()
+            ListBox1.SetItemCheckState(index, CheckState.Checked)
+        Next
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        For index = 0 To ListBox1.Items.Count - 1
+            newcoder.OverWrite = False
+            newcoder.SourceFile = ListBox1.Items.Item(index)
+            newcoder.Format = newcoder.Format_MP3
+            newcoder.AudioCodec = newcoder.AudioCodec_mp3
+            newcoder.Video_Codec = newcoder.Vcodec_NONE
+            newcoder.Threads = 0
+            newcoder.OverWrite = True
+            newcoder.ConstantRateFactor = 17
+            newcoder.RateControl = newcoder.RateControl_CRF
+            newcoder.Libx264_Preset_pass1 = newcoder.libx264_fast
+            newcoder.OutputPath = outfolder
+
+
+            newcoder.Encode()
+            ListBox1.SetItemCheckState(index, CheckState.Checked)
+        Next
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim outpath As New FolderBrowserDialog
+        If outpath.ShowDialog = Windows.Forms.DialogResult.OK Then
+            outfolder = outpath.SelectedPath
+            TextBox1.Text = outpath.SelectedPath
+        End If
+    End Sub
+
+    Private Sub MenuStrip1_ItemClicked(sender As Object, e As ToolStripItemClickedEventArgs)
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If (File.Exists("LC2Update.exe")) Then
+            Process.Start("LC2Update.exe http://www.letztechance.org/download/LC2FFMPEGEncoder.xml 1000")
+            Close()
+        Else
+            MessageBox.Show("Error: " + Environment.NewLine + "L2Update.exe " + Environment.NewLine + Environment.NewLine + "LC Update Manager not found.", "Error")
+
+
+        End If
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Close()
+
+    End Sub
+End Class
